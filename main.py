b81d9091a7667b1abce2c3f9832db5bbfa4ce49c
Bibhas <bd@nuiteq.com>
2023-09-01 14:31:44 +0530

    Fixed template

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index2.html')
+    return render_template('index.html')
 
 @app.route('/parse_sms', methods=['POST'])
 def parse_sms():
